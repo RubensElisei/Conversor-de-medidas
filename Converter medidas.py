f50d51426515515d9912877f0038a9e98319cd7e
Rubens Elisei <82228316+RubensElisei@users.noreply.github.com>
2023-06-13 15:09:54 -0300

    Add files via upload

---
+++ Converter medidas.py
@@ -0,0 +1,7 @@
+print('Olá caro ser humano')
+n1 = int(input('Digite um valor em metros:'))
+s = n1 * 100
+n = n1 / 1000.0
+t = n1 * 10
+r = n1 * 1000
+print('O valor:{} \ncm:{} \nkm:{} \ncm:{} \nmm:{}'.format(n1,s,n,t,r))
